b684ad8b65b73851e6f8b040c9712b56743c6239
Pavin <kavin0702@163.com>
2019-09-14 22:22:43 +0800

    fix prompt display error

--- src/main.c
+++ src/main.c
@@ -14,11 +14,10 @@ int main(int argc, char *argv[]) {
 
 	char prompt[50];
 	sprintf(prompt, "\033[32;1m[%d]>\033[0m ", ++lineNum); // create a promt
-	fputs(prompt, stdout);
 
 	using_history();
 	signal(SIGINT, clean_and_exit); // To clean up temporary files when interrupted
-    while((stc = readline(NULL)) != NULL) {
+    while((stc = readline(prompt)) != NULL) {
 		add_history(stc); // save the history
 		int runFlag = cinter_write(stc); // flag to determine whether to build and run.
 		if(runFlag)
@@ -26,10 +25,9 @@ int main(int argc, char *argv[]) {
 		free(stc);
 		
 		sprintf(prompt, "\033[32;1m[%d]>\033[0m ", ++lineNum); // Update prompt
-		fputs(prompt, stdout);
 		if(tabNum > 1) {
 			for(int i=0; i<tabNum-1; i++)
-				printf("····");
+				sprintf(prompt, "%s%s", prompt, "····");
 		}
 	}
 	
